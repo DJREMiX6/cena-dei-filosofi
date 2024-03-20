41a4f7d8a80f2b48ac8dcc493daee7cddac2a4d5
DJREMiX6 <francescogirellilime@gmail.com>
2024-03-20 23:37:39 +0100

    Fixed gcc -I argument

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,5 +1,5 @@
 #use "chmod a+rwx ./build-and-run.sh" to give the script the necessary permissions to be executed
 rm -r ./out ; \
 mkdir ./out/ && \
-gcc ./src/main.c ./src/argsint.c -i ./src/ -o ./out/Filosofi && \
+gcc ./src/main.c ./src/argsint.c -I ./src/ -o ./out/Filosofi && \
 ./out/Filosofi -c 8
